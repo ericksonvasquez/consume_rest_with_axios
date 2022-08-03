19f508ddd231c972a899ecda6174446c71e78190
Erickson Vásquez <45046723+ericksonvasquez@users.noreply.github.com>
2022-08-03 11:30:15 -0400

    Create consume.js

---
+++ consume.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    const consumePost = (theurl, user, pass) => {
+    const axios = require('axios');
+    const url = theurl;
+    const data = {
+        user:{
+          username: user,
+          pass: pass
+             }
+       };
+    axios.post(url, data).then(response => {
+       
+            return res.json(response.data);
+        
+        
+    });
+}
+consumePost('api_url',process.env.USER,process.env.PASS );
+});
+app.listen(process.env.PORT);
+
